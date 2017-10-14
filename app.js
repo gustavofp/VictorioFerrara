07beb9502a68ae80bb8c8564bb02a528b9148708
Gustavo Poletto <gu.fpoletto@gmail.com>
2017-10-14 01:33:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const config = require('./config.js')
+const request = require('request')
+const translate = require('google-translate-api');
+require('dotenv').config()
+
+const twitterAPI = require('node-twitter-api');
+
+const twitter = new twitterAPI({
+    consumerKey: process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+    callback: 'http://yoururl.tld/something'
+});
+
+const TOKEN = process.env.TOKEN
+const TOKEN_SECRET = process.env.TOKEN_SECRET
+
+const tweets = {}
+
+const postStatus = status => {
+    twitter.statuses("update", {
+            status
+        },
+        TOKEN,
+        TOKEN_SECRET,
+        (error, data, response) => {
+            if (error) {
+              console.log('error:', error)
+            } else {
+                console.log('status posted')
+            }
+        }
+    );
+}
+
+const verifyTweet = tweet => {
+    if(tweet == tweets.last){
+        return false
+    }else {
+        return true
+    }
+}
+
+const translateTweet = tweet => {
+    return translate(tweet, {from: 'pt', to: 'it'})
+    .then(res => res.text)
+    .catch(err => err);
+}
+
+const getTimeline = () => {
+    let isNewTweet = false
+    let tweet
+
+    twitter.getTimeline("user_timeline", {
+            screen_name: 'victorlfc_',
+            include_entities: true,
+            include_rt: false,
+            count: 1
+        },
+        TOKEN,
+        TOKEN_SECRET,
+        (error, data, response) => {
+            if (!tweets.last){
+                tweets.last = data[0].text
+                translateTweet(data[0].text).then(res => {
+                    postStatus(res)
+                })
+            }else {
+                isNewTweet = verifyTweet(data[0].text)
+                if(isNewTweet){
+                    translateTweet(data[0].text).then(res => {
+                        postStatus(res)
+                        tweets.last = data[0].text
+                    })
+                }
+
+            }
+        }
+    );
+
+}
+setInterval(() => { 
+    getTimeline()
+}, 60000)
---
+++ config.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');   
+const bodyParser = require('body-parser');
+
+const port = 8080;           
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
